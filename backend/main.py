4656af4cf8e52d3ef448b3ca93bcab11632116ff
daniil7 <daniil.k@mail.ru>
2024-05-17 23:41:14 +0500

    add option to disable swagger

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
+import os
 from fastapi import Depends, FastAPI, HTTPException
-
 from . import schemas
 
-app = FastAPI()
+if os.environ.get("DISABLE_SWAGGER") == "true":
+    docs_url = None
+    redoc_url = None
+else:
+    docs_url = "/docs"
+    redoc_url = "/redoc"
+
+app = FastAPI(
+    docs_url=docs_url,
+    redoc_url=redoc_url
+)
 
 @app.post("/test/")
 def post_test():

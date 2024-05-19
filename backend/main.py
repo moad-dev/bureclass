de6e16e17307bbf205ba907f7ce12a130f9045d4
Albert Fazlyev <albert.fz@yandex.ru>
2024-05-19 08:30:58 +0500

    fix code

--- backend/main.py
+++ backend/main.py
@@ -110,7 +110,7 @@ def search(object_name: str, limit: int):
     response = search.execute();
     return [
         schemas.Material(
-            code=hit['_source']['full_code'],
+            code=hit['_source']['okpd2'] + '.' + hit['_source']['code'],
             object_name=hit['_source']['name'],
             score=hit["_score"]
         )
